608fd7834bc12a5cfde321a0c3c8d1300f82e8e3
Steven Starnes <starnes81@gmail.com>
2017-09-25 13:43:38 -0500

    Built post index
    Built basic post index component and removed test code, deleted app.js out of components.

--- src/index.js
+++ src/index.js
@@ -4,28 +4,19 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // just for testing
 
-class Hello extends React.Component {
-	render() { return <div>Hello!</div> }
-}
 
-// just for testing
-
-class Goodbye extends React.Component {
-	render() { return <div>Goodbye!</div> }
-}
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     <div>
-		<Route path="/hello" component={Hello} />
-		<Route path="/goodbye" component={Goodbye} />  
+
 	</div>  	
     </BrowserRouter>
   </Provider>
