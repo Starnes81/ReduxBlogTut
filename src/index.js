16e4f5de854f21740019a286163a2eccd213ccc0
Steven Starnes <starnes81@gmail.com>
2017-09-26 11:58:30 -0500

    setup post show
    Start component for individual post view

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from "./components/posts_index";
 import PostsNew from "./components/posts_new";
+import PostsSow from "./components/posts_show";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -20,6 +21,7 @@ ReactDOM.render(
     <div>
     	<Switch>
 	    	<Route path="/posts/new" component={PostsNew} />
+        <Route path="/posts/:id" component={PostsSow} />
 	    	<Route path="/" component={PostsIndex} />
     	</Switch>
 	</div>  	
